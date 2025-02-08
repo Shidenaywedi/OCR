4d0f15fd685d6ee77c2a72484a34f55caae04a93
Shidenaywedi <shidengebre10@gmail.com>
2025-02-08 10:14:41 -0800

    fied commit

--- ocr/api/index.py
+++ ocr/api/index.py
@@ -5,7 +5,11 @@ import io
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/ocr": {"origins": "*"}})  # Allow requests from all origins
+
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'tiff', 'bmp', 'gif'}
 
